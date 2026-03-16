fdf7e4028d4730fda0eb55a056a0de1e3e878c5c
Pascal-1113929 <9018437@student.zadkine.nl>
2026-03-16 12:32:38 +0100

    changed slig for 16/1800

--- lib/slug.ts
+++ lib/slug.ts
@@ -1,7 +1,9 @@
 export const slugify = (text: string) =>
     text
         .replace(/\s+/g, "_")  
+        .replace(/\//g, "--")
 
 export const unslugify = (slug: string) =>
     slug
-        .replace(/_/g, " ")
+        .replace(/--/g, "/")
+        .replace(/_/g, " ");
